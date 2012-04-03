44c49873bb9e0d34ee2a7d04824aadb2cd847514
mlesniew <mlesniew@d392a5d0-1ced-4022-9bfb-244371b4f35e>
2012-04-03 21:27:56 +0000

    Changes: * Fix: the tray menu now disappears when the user clicks outside the menu (and it also doesn't break other pop-up tray menus) * Feature: after changing one of the settings all windows with a files view are now refreshed: Desktop, Explorer and Open/Close dialogs.

--- trunk/HiFiTo/resource.h
+++ trunk/HiFiTo/resource.h
@@ -1,20 +1,3 @@
-/*
- *   HiFiTo: Hidden file toggler - hides or show hidden files using a hotkey.
- *   Copyright (C) 2012  Micha³ Leœniewski
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #ifndef IDC_STATIC
 #define IDC_STATIC (-1)
 #endif
@@ -23,6 +6,7 @@
 #define IDD_ABOUT                               103
 #define IDD_SETUP                               108
 #define IDI_ABOUT                               113
+#define VERSION                                 114
 #define IDC_CHECKBOX_HIDDEN                     1005
 #define IDC_CHECKBOX_EXTENSIONS                 1006
 #define IDC_HOTKEY_EXTENSIONS                   1007
